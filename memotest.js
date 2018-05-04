675cd448a79971134725b9cb81471b5f79cda8bf
Den <den@MacBook-Air-de-Daniel.local>
2018-05-03 21:57:16 -0300

    funcionalidades nuevas

--- memotest.js
+++ memotest.js
@@ -1,6 +1,14 @@
 let arrCards = [];
 let arr = [0,1,2,3,4,5,6,7,8,9,10,11];
 let newArr = [];
+
+$('.fichas').each(function(){
+	const imgfront = "<img src='img/front-img.jpg'/>";
+	let index = $('.fichas').index(this);
+	$('.fichas').eq(index).append(imgfront);
+})
+
+
 function shuffleImg (a) {
 
 for(var i = arr.length; i; i--){
@@ -10,7 +18,7 @@ for(var i = arr.length; i; i--){
 	arr[index] = num;
 	
 	}
-console.log (arr); 
+// console.log (arr); 
 return arr
 // newArr = shuffleImg(newArr);
 // console.log(newArr)
@@ -18,49 +26,69 @@ return arr
 arr=shuffleImg(arr);
 
 function addImg (a) {
-	const length = 12
+	// const length = 12
 
-	for(var i = 0; i<length; i++){
-		if (i<=3) {
-			$('.fichas-container-back').each(function(){
-				const img = "<div class='fichas-back'>"+"<img src='img/img1.jpeg'/>" + "</div>"
-				$(this).append(img);
-				const card = $('.fichas-back')[i];
-				arrCards.push(card);
-			})
-			console.log(arrCards);
+	// for(var i = 0; i<length; i++){
+		
+			$('.fichas').each(function(i) {
+				if (i<=3) {
+					const img = "<img class='fichas-back' src='img/img1.jpeg'/>";
+					console.log($(this))
+					$(this).append(img);
+					console.log($(this))
+					
+					const card = $('.fichas-back')[i];
+					arrCards.push(card);
+				}else if (i>=4 && i<8) {
+					const img2 = "<img class='fichas-back' src='img/img2.jpeg'/>";	
+					$(this).append(img2);
+					const card = $('.fichas-back')[i];
+					arrCards.push(card);
+				}else {
+					const img3 = "<img class='fichas-back' src='img/img3.jpeg'/>";
+					$(this).append(img3);
+					const card = $('.fichas-back')[i];
+					arrCards.push(card);
+				}
 
-		} else if (i>=4 && i<8) {
-				$('.fichas-container-back').each(function(){
-				const img = "<div class='fichas-back'>"+"<img src='img/img2.jpeg'/>" + "</div>"	
-				$(this).append(img);
-				const card = $('.fichas-back')[i];
-				arrCards.push(card);
 			})
-			// console.log(arrCards[i],i)
-			
-		} else {
-			$('.fichas-container-back').each(function(){
-				const img = "<div class='fichas-back'>"+"<img src='img/img3.jpeg'/>" + "</div>"
-				$(this).append(img);
-				const card = $('.fichas-back')[i];
-				arrCards.push(card);
-			})
-		} 
-			}		
-			console.log(arrCards);
 			return arrCards;
+		// 	// console.log(arrCards);
+
+		// } else if (i>=4 && i<8) {
+		// 		$('.fichas').each(function(){
+		// 		const img = "<img class='fichas-back' src='img/img2.jpeg'/>";	
+		// 		$(this).append(img);
+		// 		const card = $('.fichas-back')[i];
+		// 		arrCards.push(card);
+		// 	})
+		// 	// console.log(arrCards[i],i)
+			
+		// } else {
+		// 	$('.fichas').each(function(){
+		// 		const img = "<img class='fichas-back' src='img/img3.jpeg'/>";
+		// 		$(this).append(img);
+		// 		const card = $('.fichas-back')[i];
+		// 		arrCards.push(card);
+		// 	})
+		// } 
+			// }		
+			// console.log(arrCards);
+			
 			// let cards = arrCards;
 			// cards = shuffle(cards,position);	
 			// shuffleImg(arrCards);	
 			// 	shuffle(arr,arrCards);
 
 }
+arrCards=addImg(arrCards)
+// $('.fichas-back').addClass("notshow");
+// console.log(arrCards);
 
-function shuffle () {
+function shuffle (a,b) {
 	// length1 = arrCards.length
 	// console.log(length1);
-	console.log(arr);
+	// console.log(arr);
 	for(var i = 0; i < arr.length; i++){
 		// console.log(arrCards[i],'a')
 		// console.log(arr[i],'b')
@@ -72,14 +100,65 @@ function shuffle () {
 		// console.log(j);
 		// console.log(arr[i]);
 	}
-	console.log(newArr,'a')	 
+	return newArr;
+	// console.log(newArr,'a')	 
 }
+newArr=shuffle(arr,arrCards)
+// console.log(newArr);
 
-$(document).on('click','.pepe', function(){
+$(document).on('click','.pepe', function (){
+	// console.log('hola')
+	
+	// $('.fichas-container-front').addClass("notshow");
+	// $('.fichas-container-back').removeClass("notshow");
+	// for(var i = 0; i < newArr.length; i++){
+	// 	console.log(newArr[i]);
+
+	// }
+	// console.log(newArr[1]);
+})
+
+function cargar (a){
 	console.log('hola')
 	for(var i = 0; i < newArr.length; i++){
-		$('.fichas-container-back').append(newArr[i]);
+		$('.fichas-container').append(newArr[i]);
 	}
+	return newArr
+}
+
+newArr = cargar(newArr);
+// console.log(newArr)
+
+
+$(document).on('click','.fichas-front',function(){
+	console.log('muerte');
+	// console.log(newArr)
+	// console.log(newArr[1]);
+	var index = $('.fichas-front').index(this);
+
+	
+	// console.log(index)
+		// console.log(newArr)
+		// console.log($('.fichas-back').eq(index))
+		$('.fichas-front').eq(index).replaceWith($('.fichas-back').removeClass("notshow"))
+		// $('.fichas-front').eq(index).addClass("notshow");
+		// $('.fichas-back').eq(index).removeClass('notshow');
+	
+
+		// $(this).find('.fichas-front').replaceWith('hola');
+		// $('.fichas-front', this).removeClass("img-front")
+		// $('.fichas-container-back').removeClass("notshow");
+
 
 })
+
+
+
+// for(var i = 0; i < index; i++){
+	// 	console.log ('')
+	// 	let arrFront =[]
+	// 	arrFront = $('.fichas-front').eq(index)
+	// 	return arrFront;
+	// }
+	// console.log(arrFront);
 
